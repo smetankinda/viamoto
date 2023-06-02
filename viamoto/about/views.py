d8815a947b3befd8443449c0dd7cbe7d02f93364
Dmitry Smetankin <smetankinda@gmail.com>
2023-06-02 22:03:17 +0300

    Настройка приложения about

--- viamoto/about/views.py
+++ viamoto/about/views.py
@@ -2,11 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-# Главная страница
 def index(request):
     return HttpResponse('Главная страница')
 
 
-# Страница со списком мороженого
 def about(request):
     return HttpResponse('О проекте')
