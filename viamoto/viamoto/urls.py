407ce638a0ca7914703648f71386b6665ced9dac
Dmitry Smetankin <smetankinda@gmail.com>
2023-06-07 22:38:49 +0300

    Media

--- viamoto/viamoto/urls.py
+++ viamoto/viamoto/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 handler404 = 'core.views.page_not_found'
 
@@ -25,3 +27,9 @@ urlpatterns = [
     path('auth/', include('users.urls')),
     path('auth/', include('django.contrib.auth.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+
