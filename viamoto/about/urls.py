e624780c9193b9c7a92ed665c4c8a96a7d99825e
Dmitry Smetankin <smetankinda@gmail.com>
2023-06-27 22:07:44 +0300

    bug fix

--- viamoto/about/urls.py
+++ viamoto/about/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'about'
 
 urlpatterns = [
-    path('', views.index.as_view(), name='index'),
-    path('about/', views.about.as_view(), name='about'),
+    path('', views.Index.as_view(), name='index'),
+    path('about/', views.About.as_view(), name='about'),
     path('profile/<str:username>/', views.profile, name='profile'),
 ]
